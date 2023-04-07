b09049932dff6bf29446ccdbb1949e2ee9959e29
= <gabrielvallejo2000@gmail.com>
2023-04-07 15:19:22 -0500

    imsgrd

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,16 @@
 $(function(){
+  $(document).on('click', '[refnav]', function() {
+    // Obtener el valor del atributo "refnav" del elemento
+    const link = $(this).attr('refnav');
+    
+    // Si el valor del atributo no está vacío, concatenar el valor a la URL
+    // de lo contrario, utilizar solo la URL base
+    const url = link ? `http://localhost/proyectos/constructora/${link}` : 'http://localhost/proyectos/constructora/';
+    
+    // Redireccionar a la URL
+    window.location.href = url;
+  });
+  
 async function getViews(fileUrls) {
   try {
     const responses = await Promise.all(fileUrls.map((fileUrl) => {
@@ -40,15 +52,27 @@ function generarParamsUrl(params) {
   return urlParams.slice(0, -1);
 }
 
-function buscar() {
+function buscar(predefinied = 0) {
+  console.log(predefinied);
   let search = $('.busqueda_input').val();
   let tipo = $('[name="tipo_venta"]:checked').val();
+  let tipo_prop = $('[name="tipo_prop"]:checked').val();
+  min = $('#minPrice').val();
+  max = $('#maxPrice').val();
+  minArea = $('#area_minima').val();
+  maxArea = $('#area_maxima').val();
+  if(predefinied == 'comprar'){
+    $('[value="1"][name="tipo_venta"]').prop('checked', true); tipo = 1;
+  } 
+  if(predefinied == 'rentar'){
+    $('[value="2"][name="tipo_venta"]').prop('checked', true); tipo = 2;
+  } 
   let params = {
-    min_precio: 1,
-    max_precio: 1000,
-    min_mts: 0,
-    max_mts: 99999,
-    id_tipo_propiedad: 1,
+    min_precio: min,
+    max_precio: max,
+    min_mts: minArea,
+    max_mts: maxArea,
+    id_tipo_propiedad: tipo_prop,
     id_tipo_venta: tipo,
     busqueda: search
   };
@@ -67,9 +91,9 @@ $.ajax({
     html += `
     <div class="col-12 mb-4">
         <div class="card">
-          <div class="row no-gutters align-items-center">
+          <div class="row d-flex align-items-center">
             <div class="col-md-5">
-              <img src="https://via.placeholder.com/400x400" class="card-img" alt="Imagen">
+              <img src="http://localhost/proyectos/constructora/img/propiedades/${objeto.ruta_imagen}" class="card-img" alt="Imagen">
             </div>
             <div class="col-md-7">
               <div class="card-body d-flex justify-content-between pb-0">
@@ -79,7 +103,7 @@ $.ajax({
               <div class="card-body">
                 <h5 class="card-title bold24">${objeto.descripcion_propiedad}</h5>
                 <p class="card-text"><svg width="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"></path></svg> ${objeto.direccion_propiedad}</p>
-                <a refnav="propiedad/${objeto.id_propiedad}" class="btn btn-primary btn-block w-100 moreinfo">Más información</a>
+                <a refnav="propiedad?id=${objeto.id_propiedad}" class="btn btn-primary btn-block w-100 moreinfo">Más información</a>
               </div>
             </div>
           </div>
@@ -129,22 +153,7 @@ function global(){
     });
     
 
-     // Seleccionar todos los elementos con la clase "refnav"
-  const refnavElements = document.querySelectorAll('[refnav]');
-  // Iterar sobre los elementos y añadirles un event listener al hacer click
-  refnavElements.forEach((element) => {
-    element.addEventListener('click', () => {
-      // Obtener el valor del atributo "data-link" del elemento
-      const link = element.getAttribute('refnav');
-  
-      // Si el valor del atributo no está vacío, concatenar el valor a la URL
-      // de lo contrario, utilizar solo la URL base
-      const url = link ? `http://localhost/proyectos/constructora/${link}` : 'http://localhost/proyectos/constructora/';
-  
-      // Redireccionar a la URL
-      window.location.href = url;
-    });
-  });
+     
 
   // Select all elements with the "externalref" attribute
   const externalRefs = document.querySelectorAll("[externalref]");
@@ -189,6 +198,18 @@ class PaginaFunciones {
     cambiarFondo();
     buscar();
   }
+  async rentar() {
+    const html = await getViews(['nav','top-page','footer']);
+    setMainContent(html);
+    cambiarFondo();
+    buscar('rentar');
+  }
+  async comprar() {
+    const html = await getViews(['nav','top-page','footer']);
+    setMainContent(html);
+    cambiarFondo();
+    buscar('comprar');
+  }
 
   async propiedad() {
     const html = await getViews(['nav','propiedad','footer']);
@@ -212,9 +233,20 @@ class Pagina {
     global();
   }
 }
-
-$(document).on('click', '[name="tipo_venta"]', function() {
-  buscar();
+let timeoutId;
+$(document).on('input', '[name="tipo_venta"],[name="tipo_prop"],.propiedad_cont input', function() {
+  $('#prop_html').html(
+    `<div class="row justify-content-center d-flex mt-5">
+      <span class="loader"></span>
+    </div>
+    <div class="row justify-content-center d-flex mt-5" style="font-size:2em; font-family: bold;">
+      Buscando propiedades...
+    </div>`
+  );
+  // Clear any previous timeouts
+  clearTimeout(timeoutId);
+  // Start a new timeout to call buscar() after 600ms
+  timeoutId = setTimeout(buscar, 600)
 });
 
 
